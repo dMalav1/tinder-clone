652e7c13e9091425f756a79fc18caa8b1782ceb0
Dheerendra Malav <dheerendra.malav@outlook.com>
2021-01-09 01:24:48 +0530

    u2

--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import "./Header.css";
+import PersonIcon from "@material-ui/icons/Person";
+import IconButton from "@material-ui/core/IconButton";
+import ForumIcon from "@material-ui/icons/Forum";
 
 function Header() {
     return (
-        <div className = 'header'>
-            <h2>I am a header</h2>
-        </div>
-    )
+      <div className="header">
+        {/*<h2>I am a header</h2>*/}
+        <IconButton>
+          <PersonIcon fontSize="large" className="header__icon" />
+        </IconButton>
+
+        <img
+          className="header__logo"
+          src="https://tinder.com/static/tinder.png"
+          alt=""
+        />
+
+        <IconButton>
+          <ForumIcon fontSize="large" className="header__icon" />
+        </IconButton>
+      </div>
+    );
 }
 
 export default Header
