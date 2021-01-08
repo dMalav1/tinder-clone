c559587443fbbf89469cb643e03da2388add6727
Dheerendra Malav <dheerendra.malav@outlook.com>
2021-01-09 00:53:31 +0530

    u1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Header from "./Header";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    //bem class naming convention
+    <div className="app">
+      {/*<h1>Lets build the mern tinder clone</h1>*/}
+
+      {/*Header*/}
+      <Header />
+
+      {/*TinderCards*/}
+      {/*SwipeButtons*/}
     </div>
   );
 }
---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./Header.css";
+
+function Header() {
+    return (
+        <div className = 'header'>
+            <h2>I am a header</h2>
+        </div>
+    )
+}
+
+export default Header
